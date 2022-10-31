057a1a60a86dbe75d95bfec160849b73b2ff71d0
Naoki Kanno <kion2k@gmail.com>
2022-10-31 22:39:59 +0900

    クランチ圧縮と異方性フィルタリングレベルを設定可能にした レイアウトをグループ化した テクスチャの最大サイズを4096までに変更 PCのテクスチャサイズデフォルト値を2048に変更

--- Assets/ClusterWorldTools/Editor/TextureImportSettings.cs
+++ Assets/ClusterWorldTools/Editor/TextureImportSettings.cs
@@ -11,12 +11,20 @@ namespace ClusterCreatorCommunity
         public const string PREFS_KEY_TEXTURE_IMPORT_FORMAT_SELECTED = PREFS_KEY_PREFIX + "TextureImportFormat";
         public const string PREFS_KEY_APPLY_TEXTURE_LIMIT = PREFS_KEY_PREFIX + "ApplyTextureLimit";
 
-        public static int MAX_TEXTURE_SIZE_PC = 1024;
-        public static int TEXTURE_SIZE_PC_INDEX = 5;
+        public static int MAX_TEXTURE_SIZE_PC = 2048;
+        public static int TEXTURE_SIZE_PC_INDEX = 6;
         public static int MAX_TEXTURE_SIZE_MOBILE = 1024;
         public static int TEXTURE_SIZE_MOBILE_INDEX = 5;
-        public static readonly string[] TEXTURE_SIZE_LIST = { "32", "64", "128", "256", "512", "1024", "2048" };
+        public static readonly string[] TEXTURE_SIZE_LIST = { "32", "64", "128", "256", "512", "1024", "2048", "4096"};
         public static bool APPLY_TEXTURE_LIMIT = false;
+
+        public const string PREFS_KEY_CRUNCHED_COMPRESSION = PREFS_KEY_PREFIX + "CrunchedCompression";
+        public const string PREFS_KEY_CRUNCHED_COMPRESSION_QUALITY = PREFS_KEY_PREFIX + "CrunchedCompressionQualtiy";
+        public const string PREFS_KEY_TEXTURE_ANISO_LEVEL = PREFS_KEY_PREFIX + "TextureAnisoLevel";
+        public static bool CRUNCHED_COMPRESSION = true;
+        public static int CRUNCHED_COMPRESSION_QUALITY = 50;
+        public static int TEXTURE_ANISO_LEVEL = 8;
+
 
         public static TextureImporterFormat TEXTURE_IMPORTER_FORMAT = TextureImporterFormat.ASTC_8x8;
         public static int TEXTURE_IMPORTER_FORMAT_INDEX = 1;
@@ -43,6 +51,10 @@ namespace ClusterCreatorCommunity
             TEXTURE_IMPORTER_FORMAT = TextureFormatIndex2TextureFormat(TEXTURE_IMPORTER_FORMAT_INDEX);
 
             APPLY_TEXTURE_LIMIT = EditorPrefs.GetBool(PREFS_KEY_APPLY_TEXTURE_LIMIT, false);
+
+            CRUNCHED_COMPRESSION = EditorPrefs.GetBool(PREFS_KEY_CRUNCHED_COMPRESSION, true);
+            CRUNCHED_COMPRESSION_QUALITY = EditorPrefs.GetInt(PREFS_KEY_CRUNCHED_COMPRESSION_QUALITY, 50);
+            TEXTURE_ANISO_LEVEL = EditorPrefs.GetInt(PREFS_KEY_TEXTURE_ANISO_LEVEL, 8);
         }
     }
 
@@ -61,32 +73,73 @@ namespace ClusterCreatorCommunity
         void OnGUI()
         {
             EditorGUILayout.LabelField("設定は全てのプロジェクトで共有されます");
-            int textureSizePCIndex = EditorGUILayout.Popup("最大サイズ（PC）", ClusterTextureImportSettings.TEXTURE_SIZE_PC_INDEX, ClusterTextureImportSettings.TEXTURE_SIZE_LIST);
-            if (textureSizePCIndex != ClusterTextureImportSettings.TEXTURE_SIZE_PC_INDEX)
-            {
-                EditorPrefs.SetInt(ClusterTextureImportSettings.PREFS_KEY_TEXTURE_SIZE_PC_SELECTED, textureSizePCIndex);
-                ClusterTextureImportSettings.TEXTURE_SIZE_PC_INDEX = textureSizePCIndex;
-                ClusterTextureImportSettings.MAX_TEXTURE_SIZE_PC = System.Convert.ToInt32(ClusterTextureImportSettings.TEXTURE_SIZE_LIST[textureSizePCIndex]);
-                settingChanged = true;
-            }
 
-            int textureSizeMobileIndex = EditorGUILayout.Popup("最大サイズ（モバイル）", ClusterTextureImportSettings.TEXTURE_SIZE_MOBILE_INDEX, ClusterTextureImportSettings.TEXTURE_SIZE_LIST);
-            if (textureSizeMobileIndex != ClusterTextureImportSettings.TEXTURE_SIZE_MOBILE_INDEX)
+            EditorGUILayout.LabelField("共通");
+            EditorGUILayout.BeginVertical(GUI.skin.box);
             {
-                EditorPrefs.SetInt(ClusterTextureImportSettings.PREFS_KEY_TEXTURE_SIZE_MOBILE_SELECTED, textureSizeMobileIndex);
-                ClusterTextureImportSettings.TEXTURE_SIZE_MOBILE_INDEX = textureSizeMobileIndex;
-                ClusterTextureImportSettings.MAX_TEXTURE_SIZE_MOBILE = System.Convert.ToInt32(ClusterTextureImportSettings.TEXTURE_SIZE_LIST[textureSizeMobileIndex]);
-                settingChanged = true;
+                bool crunchedCompression = EditorGUILayout.Toggle("クランチ圧縮(共通)", ClusterTextureImportSettings.CRUNCHED_COMPRESSION);
+                if (crunchedCompression != ClusterTextureImportSettings.CRUNCHED_COMPRESSION)
+                {
+                    EditorPrefs.SetBool(ClusterTextureImportSettings.PREFS_KEY_CRUNCHED_COMPRESSION, crunchedCompression);
+                    ClusterTextureImportSettings.CRUNCHED_COMPRESSION = crunchedCompression;
+                    settingChanged = true;
+                }
+                if (crunchedCompression)
+                {
+                    int crunchCompressionQuality = (int)EditorGUILayout.Slider("クランチ圧縮品質(共通)", ClusterTextureImportSettings.CRUNCHED_COMPRESSION_QUALITY, 0, 100);
+                    if (crunchCompressionQuality != ClusterTextureImportSettings.CRUNCHED_COMPRESSION_QUALITY)
+                    {
+                        EditorPrefs.SetInt(ClusterTextureImportSettings.PREFS_KEY_CRUNCHED_COMPRESSION_QUALITY, crunchCompressionQuality);
+                        ClusterTextureImportSettings.CRUNCHED_COMPRESSION_QUALITY = crunchCompressionQuality;
+                        settingChanged = true;
+                    }
+                }
+                int textureAnisoLevel = (int)EditorGUILayout.Slider("異方性フィルタリング(共通)", ClusterTextureImportSettings.TEXTURE_ANISO_LEVEL, 0, 16);
+                if (textureAnisoLevel != ClusterTextureImportSettings.TEXTURE_ANISO_LEVEL)
+                {
+                    EditorPrefs.SetInt(ClusterTextureImportSettings.PREFS_KEY_TEXTURE_ANISO_LEVEL, textureAnisoLevel);
+                    ClusterTextureImportSettings.TEXTURE_ANISO_LEVEL = textureAnisoLevel;
+                    settingChanged = true;
+                }
             }
+            EditorGUILayout.EndVertical();
 
-            int textureFormatIndex = EditorGUILayout.Popup("圧縮（モバイル）", ClusterTextureImportSettings.TEXTURE_IMPORTER_FORMAT_INDEX, ClusterTextureImportSettings.TEXTURE_IMPORTER_FORMAT_LIST);
-            if (textureFormatIndex != ClusterTextureImportSettings.TEXTURE_IMPORTER_FORMAT_INDEX)
+            EditorGUILayout.LabelField("PC");
+            EditorGUILayout.BeginVertical(GUI.skin.box);
             {
-                EditorPrefs.SetInt(ClusterTextureImportSettings.PREFS_KEY_TEXTURE_IMPORT_FORMAT_SELECTED, textureFormatIndex);
-                ClusterTextureImportSettings.TEXTURE_IMPORTER_FORMAT_INDEX = textureFormatIndex;
-                ClusterTextureImportSettings.TEXTURE_IMPORTER_FORMAT = ClusterTextureImportSettings.TextureFormatIndex2TextureFormat(textureFormatIndex);
-                settingChanged = true;
+                int textureSizePCIndex = EditorGUILayout.Popup("最大サイズ", ClusterTextureImportSettings.TEXTURE_SIZE_PC_INDEX, ClusterTextureImportSettings.TEXTURE_SIZE_LIST);
+                if (textureSizePCIndex != ClusterTextureImportSettings.TEXTURE_SIZE_PC_INDEX)
+                {
+                    EditorPrefs.SetInt(ClusterTextureImportSettings.PREFS_KEY_TEXTURE_SIZE_PC_SELECTED, textureSizePCIndex);
+                    ClusterTextureImportSettings.TEXTURE_SIZE_PC_INDEX = textureSizePCIndex;
+                    ClusterTextureImportSettings.MAX_TEXTURE_SIZE_PC = System.Convert.ToInt32(ClusterTextureImportSettings.TEXTURE_SIZE_LIST[textureSizePCIndex]);
+                    settingChanged = true;
+                }
             }
+            EditorGUILayout.EndVertical();
+
+            EditorGUILayout.LabelField("モバイル");
+            EditorGUILayout.BeginVertical(GUI.skin.box);
+            {
+                int textureSizeMobileIndex = EditorGUILayout.Popup("最大サイズ", ClusterTextureImportSettings.TEXTURE_SIZE_MOBILE_INDEX, ClusterTextureImportSettings.TEXTURE_SIZE_LIST);
+                if (textureSizeMobileIndex != ClusterTextureImportSettings.TEXTURE_SIZE_MOBILE_INDEX)
+                {
+                    EditorPrefs.SetInt(ClusterTextureImportSettings.PREFS_KEY_TEXTURE_SIZE_MOBILE_SELECTED, textureSizeMobileIndex);
+                    ClusterTextureImportSettings.TEXTURE_SIZE_MOBILE_INDEX = textureSizeMobileIndex;
+                    ClusterTextureImportSettings.MAX_TEXTURE_SIZE_MOBILE = System.Convert.ToInt32(ClusterTextureImportSettings.TEXTURE_SIZE_LIST[textureSizeMobileIndex]);
+                    settingChanged = true;
+                }
+
+                int textureFormatIndex = EditorGUILayout.Popup("圧縮", ClusterTextureImportSettings.TEXTURE_IMPORTER_FORMAT_INDEX, ClusterTextureImportSettings.TEXTURE_IMPORTER_FORMAT_LIST);
+                if (textureFormatIndex != ClusterTextureImportSettings.TEXTURE_IMPORTER_FORMAT_INDEX)
+                {
+                    EditorPrefs.SetInt(ClusterTextureImportSettings.PREFS_KEY_TEXTURE_IMPORT_FORMAT_SELECTED, textureFormatIndex);
+                    ClusterTextureImportSettings.TEXTURE_IMPORTER_FORMAT_INDEX = textureFormatIndex;
+                    ClusterTextureImportSettings.TEXTURE_IMPORTER_FORMAT = ClusterTextureImportSettings.TextureFormatIndex2TextureFormat(textureFormatIndex);
+                    settingChanged = true;
+                }
+            }
+            EditorGUILayout.EndVertical();
 
             bool applyTextureLimit = EditorGUILayout.Toggle("制限を適用", ClusterTextureImportSettings.APPLY_TEXTURE_LIMIT);
             if(applyTextureLimit != ClusterTextureImportSettings.APPLY_TEXTURE_LIMIT)
@@ -127,6 +180,11 @@ namespace ClusterCreatorCommunity
         public static void OptimizeMainSettings(in TextureImporter importer)
         {
             importer.maxTextureSize= Mathf.Min(importer.maxTextureSize, ClusterTextureImportSettings.MAX_TEXTURE_SIZE_PC);
+
+            importer.anisoLevel = ClusterTextureImportSettings.TEXTURE_ANISO_LEVEL;
+            importer.crunchedCompression = ClusterTextureImportSettings.CRUNCHED_COMPRESSION;
+            if(importer.crunchedCompression)
+                importer.compressionQuality = ClusterTextureImportSettings.CRUNCHED_COMPRESSION_QUALITY;
         }
 
         public static void OptimizePlatformSettings(in TextureImporter importer, in string platform)
